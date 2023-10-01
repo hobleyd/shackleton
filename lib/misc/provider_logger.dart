d34374b612e72952900403f5ebebe1dfb607533c
David Hobley <david.hobley@sharpblue.com.au>
2023-10-01 16:27:19 +0100

    reset logging to default; no logging on release build

--- lib/misc/provider_logger.dart
+++ lib/misc/provider_logger.dart
@@ -8,11 +8,6 @@ var logger = Logger();
 class ProviderLogger extends ProviderObserver {
   @override
   void didUpdateProvider(ProviderBase provider, Object? previousValue, Object? newValue, ProviderContainer container,) {
-    if (provider.name == 'selectedEntitiesProvider') {
-      //debugPrint('[${provider.name ?? provider.runtimeType}] value: $newValue');
-      debugPrint('selectedEntitiesProvider(FolderList): ${container.read(selectedEntitiesProvider(FileType.folderList))}');
-      debugPrint('selectedEntitiesProvider(PreviewGrid): ${container.read(selectedEntitiesProvider(FileType.previewGrid))}');
-      //logger.d('PreviewGrid: ${container.read(selectedEntitiesProvider(FileType.previewGrid))}');
-    }
+    logger.d('[${provider.name ?? provider.runtimeType}] value: $newValue');
   }
 }
